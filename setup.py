517a29ae30a5b13a2d6500f44c22ddcbea255718
Ashwin <arihant2math@gmail.com>
2022-12-31 16:14:11 -0800

    setup.py update

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='ftc_api',
@@ -6,8 +9,10 @@ setup(
     author_email='arihant2math@gmail.com',
     license='MIT',
     description='A python client to access the FIRST Tech Challenge API',
-    version='0.0.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.0.1',
     install_requires=[
         'httpx', 'attrs', 'python-dateutil'
-    ],
+    ]
 )
