22f23e7ff1d40df679d46cce06e58b9780f7a284
Ashwin <arihant2math@gmail.com>
2022-12-31 19:59:02 -0800

    bumped version number properly

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='A python client to access the FIRST Tech Challenge API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=[
         'httpx', 'attrs', 'python-dateutil'
     ]
